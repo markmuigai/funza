eef2cc787b9d702bac83ce188f6a3012f9b652c1
markmuigai <markmuigai@gmail.com>
2020-01-18 12:15:19 +0300

    Added assessments model and migration

---
+++ database/migrations/2020_01_17_183954_create_assessments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAssessmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assessments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('Grade_Subject_id');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assessments');
+    }
+}
