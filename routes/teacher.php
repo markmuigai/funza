28702a2de8991bac795dda7f24ff1018fd18dcf3
markmuigai <markmuigai@gmail.com>
2020-01-19 20:58:44 +0300

    Updates to teacher routes

--- routes/teacher.php
+++ routes/teacher.php
@@ -16,3 +16,11 @@ Route::get('/', 'AppController@dashboard')->name('dashboard');
 
 // Classroom
 Route::get('/classroom/{classroom}', 'ClassroomController@show')->name('classroom');
+
+// Subjects
+Route::name('classroom.')->prefix('/classroom/{classroom}')->group(function () {
+    Route::get('subject/{subject}', 'SubjectController@show')->name('subject');
+});    
+
+// Classroom subject dashboard page
+Route::get('track-progress/{classroomSubject}', 'ProgressController@index');
