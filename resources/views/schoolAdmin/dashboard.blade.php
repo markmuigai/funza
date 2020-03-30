a31ba54cdeabf6de870600f43f98e195d6946a7a
markmuigai <markmuigai@gmail.com>
2020-03-31 00:58:57 +0300

    Added school dashboard blade

---
+++ resources/views/schoolAdmin/dashboard.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.schoolAdmin')
+@section('page-title', 'School Admin Dashboard')
+    
+@section('content')
+        <div class="container-fluid ">
+            Welcome to your school dashboard
+        </div>
+@endsection
