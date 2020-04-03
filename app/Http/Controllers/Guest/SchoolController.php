8f89bb1d1bf2c8971e860ffa77a0c182d958a491
markmuigai <markmuigai@gmail.com>
2020-04-03 17:36:13 +0300

    School registration backend logic

--- app/Http/Controllers/Guest/SchoolController.php
+++ app/Http/Controllers/Guest/SchoolController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers\Guest;
 
+use App\User;
+use App\School;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\Controller;
 
 class SchoolController extends Controller
@@ -33,22 +38,61 @@ class SchoolController extends Controller
      */
     public function storeSchoolDetails(Request $request)
     {
-        return redirect()->route('school.admin.create');
+        // Validation
+        $request->validate([
+            'name' => 'required', 
+            'email' => 'required', 
+            'telno' => 'required', 
+            'address' => 'required',  
+            'county' => 'required', 
+            'subcounty' => 'required' 
+        ]);
+
+        // Create school
+        $school = School::create($request->all());
+
+        // Notify school admin on school creation
+
+        return redirect()->route('school.admin.create', ['school' => $school]);
     }
 
     /**
      * Show school admin form
      */
-    public function createAdmin()
+    public function createAdmin(School $school)
     {
-        return view('guest.schoolAdmin.create');
+        return view('guest.schoolAdmin.create', [
+            'school' => $school
+        ]);
     }
 
     /**
      * Store school admin details
      */
-    public function storeSchoolAdminDetails(Request $request)
+    public function storeSchoolAdminDetails(Request $request, School $school)
     {
+        // // Validation
+        // $request->validate([
+        //     'name' => ['required', 'string', 'max:255'],
+        //     'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+        //     'password' => ['required', 'string', 'min:8', 'confirmed'],
+        // ]);
+
+        DB::transaction(function () use ($request, $school) {
+            // Create User
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+            
+            // Assign user to school
+            $school->users()->attach($user->id);
+
+            // Assign school admin role to the pivot table instances of the users selected
+            $user->schoolAdministration($school)->roles()->attach(Role::where('name','school_admin')->first()->id);
+        });
+
         return redirect()->route('school.confirm-registration.create');
     }
 
@@ -65,7 +109,7 @@ class SchoolController extends Controller
      */
     public function confirmSchoolRegistration(Request $request)
     {
-        return 'School Registered Succeddfully';
+        return redirect()->route('login');
     }
 
     /**
