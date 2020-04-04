9b27aa89cb8a1d9a20f381694089ad7fdc0b269a
markmuigai <markmuigai@gmail.com>
2020-04-04 14:06:37 +0300

    Setup school admin teacher and students routing

--- routes/schoolAdmin.php
+++ routes/schoolAdmin.php
@@ -13,3 +13,9 @@
 
 // School Admin Dashboard
 Route::get('/', 'AppController@dashboard')->name('dashboard');
+
+// Teachers
+Route::resource('teachers', 'TeacherController');
+
+// Students
+Route::resource('students', 'StudentController');
