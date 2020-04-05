2f07120cb57695add4a65f6cbaf0bbd7e41e8102
markmuigai <markmuigai@gmail.com>
2020-04-05 14:34:53 +0300

    Updates to teachers table

--- resources/views/components/schoolAdmin/tables/teachers.blade.php
+++ resources/views/components/schoolAdmin/tables/teachers.blade.php
@@ -1,12 +1,18 @@
-<table class="table p-5 bg-transaparent">
-    <tr>
-        <th>Name</th>
-        <th>Actions</th>
-    </tr>
-    @foreach ($teachers as $teacher)
-        <tr>
-            <td>{{$teacher->name}}</td>
-            <td><a href="#" class="btn btn-primary btn-sm">Teacher Portfolio</a></td>
-        </tr>
-    @endforeach
+<table class="table table-hover">
+    <tbody>
+      <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Actions</th>
+      </tr>
+      @foreach ($teachers as $teacher)
+          <tr>
+              <td>{{$teacher->name}}</td>
+              <td>
+                <a href="#" class="btn btn-primary btn-sm">View Performance</a>
+                <a href="#" class="btn btn-success btn-sm">Edit classes</a>
+              </td>
+          </tr>
+      @endforeach
+  </tbody>
 </table>
