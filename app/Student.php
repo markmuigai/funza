e69563ebc191c617c6738b523f85cef181730af6
markmuigai <markmuigai@gmail.com>
2020-06-01 12:07:19 +0300

    show results based on assessment count

--- app/Student.php
+++ app/Student.php
@@ -36,11 +36,12 @@ class Student extends Model
         return $this->classrooms()->wherePivot('status', true)->first();
     }
 
-    // Fetch outcome results for a substrand
-    public function outcomeResultsForSubstrand($substrand_id)
+    // Fetch outcome results for a substrand and a certain assessment count
+    public function outcomeResultsForSubstrand($substrand_id, $assessment_count)
     {
-        return $this->outcomeResults->filter(function($outcome_result) use($substrand_id){
-            return $outcome_result->outcome->substrand->id == $substrand_id;
+        return $this->outcomeResults->filter(function($outcome_result) use($substrand_id, $assessment_count){
+            return $outcome_result->outcome->substrand->id == $substrand_id 
+                && $outcome_result->count == $assessment_count;
         });
     }
 
@@ -51,4 +52,23 @@ class Student extends Model
                 Substrand::find($substrand_id)->outcomes->count();
     }
 
+    /**
+     * Get a students score for a substrand
+     */
+    public function substrandScore($substrand_id, $assessment_count)
+    {
+        $outcome_scores =  $this->outcomeresultsForSubstrand($substrand_id, $assessment_count)->pluck('score');
+
+        // Carry is the inital iteration value
+        // Fetch the total score 
+        $score_total = $outcome_scores->reduce(function ($carry, $score) {
+            return $carry + $score;
+        });
+
+        // fetch the number of outcomes for the substrand
+        $outcome_count = Substrand::find($substrand_id)->outcomes->count();
+
+        // Calculate the average
+        return $average_score = ($score_total/($outcome_count*5))*100;
+    }
 }
