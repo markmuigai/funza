db342213c3a2472086646e7b532edf03acb9a805
markmuigai <markmuigai@gmail.com>
2020-06-23 16:31:30 +0300

    Setup student results schema

---
+++ database/migrations/2020_06_23_130850_create_student_substrand_scores_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentSubstrandScoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_substrand_scores', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('student_id');
+            $table->unsignedInteger('substrand_id');
+            $table->unsignedInteger('score');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_substrand_scores');
+    }
+}
